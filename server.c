f4f3aab8b08bd554167456ab88209cc9519579ad
dunkthelunk <prudhvi.porandla@icloud.com>
2018-08-06 22:12:33 +0530

    recv and send

--- server.c
+++ server.c
@@ -7,44 +7,52 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
-#define PORT "8888"
+#define PORT "8863"
 #define BACKLOG 10
 
 int main() {
   struct addrinfo hints, *res;
   memset(&hints, 0, sizeof hints);
-//hints.ai_family = AF_UNSPEC;
+  hints.ai_family = AF_UNSPEC;
   hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_STREAM;
- // hints.ai_flags = AI_PASSIVE;
+  hints.ai_flags = AI_PASSIVE;
   getaddrinfo(NULL, PORT, &hints, &res);
   int socket_fd, newfd; 
-   socket_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+  socket_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
   bind(socket_fd, res->ai_addr, res->ai_addrlen);
-  
+
   void *addr;
-  char *ipver;
   unsigned short int port;
   char ipstr[INET6_ADDRSTRLEN];
   struct sockaddr_in *address = (struct sockaddr_in *)res->ai_addr;
   addr = &(address->sin_addr);
   port = address->sin_port;
   inet_ntop(res->ai_family, addr, ipstr, sizeof ipstr);
+  port = ntohs(port);
   printf("Listening at %s on port %i\n", ipstr, port);
 
   struct sockaddr_storage their_addr;
-socklen_t addr_size;
+  socklen_t addr_size;
   listen(socket_fd, BACKLOG);
-  char *msg = "Jeez was here!";
-  int len, bytes_sent;
-  len = strlen(msg);
+  char *msg1= "Jeez was here once!";
+  char *msg2= "Jeez was here twice!";
+  int bytes_sent;
+  char recvbuf[1000];
   while(1) {
     addr_size = sizeof their_addr;
     newfd = accept(socket_fd, (struct sockaddr *)&their_addr, &addr_size);
-    bytes_sent = send(newfd, msg, len, 0); 
-   // close(newfd);
+    recv(newfd, &recvbuf, 1000, 0);
+    printf("req received %s\n", recvbuf);
+    if(recvbuf[0] == '1') {
+      bytes_sent = send(newfd, msg1, strlen(msg1), 0); 
+    } else if(recvbuf[0] == '2') {
+      bytes_sent = send(newfd, msg2, strlen(msg2), 0); 
+    }
+    close(newfd);
+    printf("closed %i\n", newfd);
   }
   freeaddrinfo(res);
- // close(socket_fd);
+  close(newfd);
   return 0;
 }
